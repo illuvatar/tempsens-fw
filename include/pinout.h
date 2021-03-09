d03e983b104fdb68dc41569a656a86ad75cda0f6
Patrik Granath <eru.illuvatar@gmail.com>
2021-03-09 21:39:44 +0100

    Project setup with led-blink demo

---
+++ include/pinout.h
@@ -0,0 +1,12 @@
+#ifndef PINOUT_H_
+#define PINOUT_H_
+
+#define GPIO_1WIRE     0 // Dallas 1-Wire
+#define GPIO_DHT       2 // DHT22-sensor
+#define GPIO_I2CSDC    5 // I2C Clock pin
+#define GPIO_I2CSDA    4 // I2C Data pin
+#define GPIO_SPISCK   14 // SPI Clock
+#define GPIO_SPIMOSI  13 // SPI MOSI
+#define GPIO_SPIMISO  12 // SPI MISO
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include "pinout.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(GPIO_1WIRE, OUTPUT);
+  pinMode(GPIO_DHT, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(GPIO_1WIRE, LOW);
+  digitalWrite(GPIO_DHT, HIGH);
+  Serial.println("The BLUE led is off");
+  delay(1000);
+  digitalWrite(GPIO_1WIRE, HIGH);
+  digitalWrite(GPIO_DHT, LOW);
+  Serial.println("The led is on");
+  delay(1000);
 }
