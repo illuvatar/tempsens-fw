92576ac8b66294a755d3750cda7bc1dd63d53e6c
Patrik Granath <eru.illuvatar@gmail.com>
2021-03-09 22:04:57 +0100

    Added formatting rules to workspace

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
 #include <Arduino.h>
+
 #include "pinout.h"
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(GPIO_1WIRE, OUTPUT);
-  pinMode(GPIO_DHT, OUTPUT);
+    Serial.begin(115200);
+    pinMode(GPIO_1WIRE, OUTPUT);
+    pinMode(GPIO_DHT, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(GPIO_1WIRE, LOW);
-  digitalWrite(GPIO_DHT, HIGH);
-  Serial.println("The BLUE led is off");
-  delay(1000);
-  digitalWrite(GPIO_1WIRE, HIGH);
-  digitalWrite(GPIO_DHT, LOW);
-  Serial.println("The led is on");
-  delay(1000);
+    digitalWrite(GPIO_1WIRE, LOW);
+    digitalWrite(GPIO_DHT, HIGH);
+    Serial.println("The BLUE led is off");
+    delay(1000);
+    digitalWrite(GPIO_1WIRE, HIGH);
+    digitalWrite(GPIO_DHT, LOW);
+    Serial.println("The led is on");
+    delay(1000);
 }
